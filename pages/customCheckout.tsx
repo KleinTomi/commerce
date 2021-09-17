288907ed626c7e29bd3b829432a3ae3781f57fd9
Richard <richard.molnar@incepteam.com>
2021-09-17 14:45:44 +0200

    Pacman

--- pages/customCheckout.tsx
+++ pages/customCheckout.tsx
@@ -1,7 +1,11 @@
 import { FC } from 'react'
 
 const CustomCheckout: FC = () => {
-  return (<div>Lofasz</div>)
+  return (
+    <>
+      <iframe src={'/pacman/index.html'} style={{width:'100%', height:800}}/>
+    </>
+  )
 }
 
 export default CustomCheckout;
