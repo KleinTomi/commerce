ec90d738e49795ff3883cd240325d162233f3a9a
Vincze Márton <marton.vincze@incepteam.com>
2021-09-17 15:20:37 +0200

    Description is normal now

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,8 +58,8 @@ export default function Home({
         ))}
       </Marquee>
       <Hero
-        headline=" Dessert dragée halvah croissant."
-        description="Cupcake ipsum dolor sit amet lemon drops pastry cotton candy. Sweet carrot cake macaroon bonbon croissant fruitcake jujubes macaroon oat cake. Soufflé bonbon caramels jelly beans. Tiramisu sweet roll cheesecake pie carrot cake. "
+        headline="Wolt for frictionless e-commerce solutions by Taking the Pill"
+        description="Choose your e-commerce solution, order it and watch your futár when he takes the pill and tries to find you."
       />
       <Grid layout="B" variant="filled">
         {products.slice(0, 3).map((product: any, i: number) => (
